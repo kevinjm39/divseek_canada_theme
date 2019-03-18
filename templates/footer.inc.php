08b5ac94cc3cbdea253e94822ed9dd78237dfe2b
bradfordcondon <bradford.condon@gmail.com>
2019-03-17 20:28:24 -0400

    init

---
+++ templates/footer.inc.php
@@ -0,0 +1,18 @@
+<div class="section bg-inverse main-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4 text-center">
+                Footer Link 1
+            </div><!-- /.col -->
+            <div class="col-sm-4 text-center">
+                Footer Link 2
+            </div><!-- /.col -->
+            <div class="col-sm-4 text-center">
+                <a href="http://tripal.info" target="_blank">
+                    <img src="<?php print base_path() . path_to_theme() . '/dist/images/powered_by_tripal.png'; ?>"
+                         alt="Tripal Logo" class="img-fluid img-max-h100">
+                </a>
+            </div><!-- /.col -->
+        </div><!-- /.row -->
+    </div><!-- /.container -->
+</div><!-- /.section -->
---
+++ templates/nav.tpl.php
@@ -0,0 +1,49 @@
+<nav id="navigation"
+     class="main-header navbar navbar-light bg-faded navbar-full navbar-expand-lg">
+  <div class="container">
+    <a class="navbar-brand" href="/">
+      <div class="logo-wrapper">
+          <img src="<?php print base_path() . path_to_theme() . '/dist/images/genome_canda_logo.png'; ?>"
+          <span class="logo-text"><?php print variable_get('site_name') ?></span>
+        <div class="text-muted text-xs hidden-xs-down">
+          <?php print variable_get('site_slogan') ?>
+        </div>
+      </div>
+    </a>
+
+    <button class="navbar-toggler navbar-toggler-right"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbar-collapse"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbar-collapse">
+      <?php
+      if (!empty($primary_nav)):
+        print render($primary_nav);
+      endif;
+      ?>
+
+      <?php
+      if (!empty($secondary_nav)):
+        print render($secondary_nav);
+      endif;
+      ?>
+
+      <?php if (!user_is_logged_in()): ?>
+        <ul class="navbar-nav mr-md-auto mr-lg-0">
+          <li class="leaf nav-item">
+            <a href="/user/login" class="nav-link">Login</a>
+          </li>
+          <li class="leaf nav-item">
+            <a href="/user/register" class="nav-link">Register</a>
+          </li>
+        </ul>
+      <?php endif; ?>
+    </div>
+  </div>
+</nav> <!-- /.section, /#navigation -->
