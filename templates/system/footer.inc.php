971c9c471c4e63fca63ff3fca2a4ae94c424789d
bradfordcondon <bradford.condon@gmail.com>
2019-03-25 15:18:04 -0400

    WIP

--- templates/system/footer.inc.php
+++ templates/system/footer.inc.php
@@ -9,7 +9,7 @@
             </div><!-- /.col -->
             <div class="col-sm-4 text-center">
                 <a href="http://tripal.info" target="_blank">
-                    <img src="<?php print base_path() . path_to_theme() . '/dist/images/powered_by_tripal.png'; ?>"
+                    <img src="<?php print base_path() . drupal_get_path('theme', 'divseek') . '/dist/images/powered_by_tripal.png'; ?>"
                          alt="Tripal Logo" class="img-fluid img-max-h100">
                 </a>
             </div><!-- /.col -->
