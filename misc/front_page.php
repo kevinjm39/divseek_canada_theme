eb890f51a2082fb37582fe16b19d97a7a9b5df20
bradfordcondon <bradford.condon@gmail.com>
2019-03-25 20:27:59 -0400

    dont ignore node modules

--- misc/front_page.php
+++ misc/front_page.php
@@ -1,7 +1,7 @@
 <p class="lead">Divseek Canada lead text.</p>
 <div class="row">
   <div class="col-sm-6 col-md-4">
-    <div class="card">
+    <div class="card card-crop">
       <img class="img-responsive" src="<?php print base_path() . drupal_get_path('theme', 'divseek') . '/dist/images/lentil.jpg'; ?>" alt="Card image cap">
       <div class="card-block">
         <h4 class="card-title">
@@ -13,7 +13,7 @@
   </div>
 
   <div class="col-sm-6 col-md-4">
-    <div class="card">
+    <div class="card card-crop">
       <img class="img-responsive" src="<?php print base_path() . drupal_get_path('theme', 'divseek') . '/dist/images/flax.jpg'; ?>" alt="Card image cap" title="Card Image.">
       <div class="card-block">
         <h4 class="card-title"><a href="/bio_data/16">Flax</a></h4>
@@ -23,7 +23,7 @@
   </div>
 
   <div class="col-sm-6 col-md-4">
-    <div class="card">
+    <div class="card card-crop">
       <img class="img-responsive" src="<?php print base_path() . drupal_get_path('theme', 'divseek')  . '/dist/images/sunflowers.jpg'; ?>" alt="Card image cap" title="Card title.">
       <div class="card-block">
         <h4 class="card-title"><a href="/bio_data/10">Sunflower</a></h4>
--- templates/system/footer.inc.php
+++ templates/system/footer.inc.php
@@ -1,4 +1,4 @@
-<div class="section bg-gray main-footer">
+<div class="section bg-inverse main-footer">
     <div class="container">
         <div class="row">
             <div class="col-sm-4 text-center">
@@ -10,7 +10,7 @@
             <div class="col-sm-4 text-center">
                 <a href="http://tripal.info" target="_blank">
                     <img src="<?php print base_path() . drupal_get_path('theme', 'divseek') . '/dist/images/powered_by_tripal.png'; ?>"
-                         alt="Tripal Logo" class="img-fluid img-max-h100">
+                         alt="Tripal Logo" class="img-max-h100">
                 </a>
             </div><!-- /.col -->
         </div><!-- /.row -->
