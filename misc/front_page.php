eaf5eb75de4e32e44b1269cd4cab537863ee07c5
bradfordcondon <bradford.condon@gmail.com>
2019-03-25 20:49:15 -0400

    fix card image on narrow

--- misc/front_page.php
+++ misc/front_page.php
@@ -2,7 +2,7 @@
 <div class="row">
   <div class="col-sm-6 col-md-4">
     <div class="card card-crop">
-      <img class="img-responsive" src="<?php print base_path() . drupal_get_path('theme', 'divseek') . '/dist/images/lentil.jpg'; ?>" alt="Card image cap">
+      <img class="img-responsive card-img-top" src="<?php print base_path() . drupal_get_path('theme', 'divseek') . '/dist/images/lentil.jpg'; ?>" alt="Card image cap">
       <div class="card-block">
         <h4 class="card-title">
           <a href="/bio_data/21">Lentil</a>
@@ -14,7 +14,7 @@
 
   <div class="col-sm-6 col-md-4">
     <div class="card card-crop">
-      <img class="img-responsive" src="<?php print base_path() . drupal_get_path('theme', 'divseek') . '/dist/images/flax.jpg'; ?>" alt="Card image cap" title="Card Image.">
+      <img class="img-responsive card-img-top" src="<?php print base_path() . drupal_get_path('theme', 'divseek') . '/dist/images/flax.jpg'; ?>" alt="Card image cap" title="Card Image.">
       <div class="card-block">
         <h4 class="card-title"><a href="/bio_data/16">Flax</a></h4>
         <p class="card-text">Summary of Flax.<br /><br /></p>
@@ -24,7 +24,7 @@
 
   <div class="col-sm-6 col-md-4">
     <div class="card card-crop">
-      <img class="img-responsive" src="<?php print base_path() . drupal_get_path('theme', 'divseek')  . '/dist/images/sunflowers.jpg'; ?>" alt="Card image cap" title="Card title.">
+      <img class="img-responsive card-img-top" src="<?php print base_path() . drupal_get_path('theme', 'divseek')  . '/dist/images/sunflowers.jpg'; ?>" alt="Card image cap" title="Card title.">
       <div class="card-block">
         <h4 class="card-title"><a href="/bio_data/10">Sunflower</a></h4>
         <p class="card-text">Summary of sunflower</p>
